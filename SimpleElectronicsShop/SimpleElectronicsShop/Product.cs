0e444caa8ef2db13ce78b4d810df28bb7d82e9da
ManosMorf97 <manosmorf97@gmal.com>
2024-03-22 11:21:29 +0200

    MyFirstCommit

---
+++ SimpleElectronicsShop/SimpleElectronicsShop/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Product
+{
+	private int price;
+	private string name_title;
+	public Product(int price, string name_title)
+	{
+		this.price = price;
+		this.name_title = name_title;
+	}
+	
+	public int Price{
+		get { return price; }
+	}
+	public string Name_Title {
+		get { return name_title; }
+	}
+
+	public override string ToString()
+	{
+     
+		return "name: " + name_title + " price: " + price;
+	}
+}
---
+++ SimpleElectronicsShop/SimpleElectronicsShop/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int x = 0;
+Product p=new Product(1,"hello");
+Console.WriteLine(p);
