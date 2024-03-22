f1ef3a54d17552c4feb7890ba7fd0e534386b53e
ManosMorf97 <manosmorf97@gmal.com>
2024-03-22 11:54:34 +0200

    Better

--- SimpleElectronicsShop/SimpleElectronicsShop/Product.cs
+++ SimpleElectronicsShop/SimpleElectronicsShop/Product.cs
@@ -1,25 +1,23 @@
-﻿using System;
-
-public class Product
+﻿namespace class_product
 {
-	private int price;
-	private string name_title;
-	public Product(int price, string name_title)
-	{
-		this.price = price;
-		this.name_title = name_title;
-	}
-	
-	public int Price{
-		get { return price; }
-	}
-	public string Name_Title {
-		get { return name_title; }
-	}
+	public class Product(int price, string name_title)
+    {
+		private readonly int price=price;
+		private readonly string name_title=name_title;
 
-	public override string ToString()
-	{
-     
-		return "name: " + name_title + " price: " + price;
+		public int Price
+		{
+			get { return price; }
+		}
+		public string Name_Title
+		{
+			get { return name_title; }
+		}
+		public override string ToString()
+		{
+
+			return "name: " + name_title + "\n" + "price: " + price;
+		}
+
 	}
 }
--- SimpleElectronicsShop/SimpleElectronicsShop/Program.cs
+++ SimpleElectronicsShop/SimpleElectronicsShop/Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine("Hello, World!");
 int x = 0;
 Product p=new Product(1,"hello");
 Console.WriteLine(p);
+int showme(Product p) {
+    return 0;
+}
